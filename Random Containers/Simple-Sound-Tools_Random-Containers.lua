f853bdddf481590fd6cf03ab23ec411e20309e67
Daniel Dehaan <danielrdehaan@gmail.com>
2025-05-09 14:28:02 -0500

    Update Simple-Sound-Tools_Random-Containers.lua

--- Random Containers/Simple-Sound-Tools_Random-Containers.lua
+++ Random Containers/Simple-Sound-Tools_Random-Containers.lua
@@ -1,6 +1,6 @@
 -- @description Simple Sound Tools - Random Containers for Reaper
 -- @author Simple Sound Tools
--- @version 1.8 - BETA
+-- @version 1.9 - BETA
 -- @about
 --   This script brings the essential functionality of audio middleware to Reaper
 -- @provides
@@ -11,4 +11,4 @@ r = reaper
 SEP = package.config:sub(1, 1)
 DATA = _VERSION == 'Lua 5.3' and 'Data53' or 'Data'
 DATA_PATH = debug.getinfo(1, 'S').source:match '@(.+[/\\])' .. DATA .. SEP
-dofile(DATA_PATH .. 'Simple-Sound-Tools_Random-Container-Functions.dat')
+dofile(DATA_PATH .. 'Simple-Sound-Tools_Random-Container-Functions.lua')

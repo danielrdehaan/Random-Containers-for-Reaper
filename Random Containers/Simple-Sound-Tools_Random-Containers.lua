e908b039730e076631542aeb1cde35a3fe7f95cf
Daniel Dehaan <danielrdehaan@gmail.com>
2025-08-08 11:15:42 -0500

    Updated change log.

--- Random Containers/Simple-Sound-Tools_Random-Containers.lua
+++ Random Containers/Simple-Sound-Tools_Random-Containers.lua
@@ -1,13 +1,13 @@
 -- @description Random Containers for Reaper
 -- @author Simple Sound Tools
--- @version 2.0.B.003
+-- @version 2.0.B.004
 -- @about
---   This script brings the essential functionality of audio middleware to Reaper
+--   	This script brings the essential functionality of audio middleware to Reaper
 -- @provides
 -- 		[nomain] Fonts/Roboto/*.ttf
 -- 		[nomain] Data/*.lua
 -- @changelog
--- - Fix: Error in displaying weighted values for each take when using Weighted Take Randomization
+-- 		Fix: Error in displaying weighted values for each take when using Weighted Take Randomization
 SCRIPT_FOLDER = 'Random Containers'
 r = reaper
 SEP = package.config:sub(1, 1)
